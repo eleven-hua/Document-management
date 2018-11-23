1e73828a9a64ea1bd7eacc64755ca782fe4987af
jsonhua <17710120608@163.com>
2018-11-23 14:21:54 +0800

    Update views.py

--- file/views.py
+++ file/views.py
@@ -7,7 +7,6 @@ from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect, QueryDict, JsonResponse, FileResponse
 from django.shortcuts import render,reverse,redirect
 from django.utils.encoding import escape_uri_path
-
 from file import models
 from django.contrib.auth.views import login_required
 import re
@@ -25,27 +24,20 @@ def fileload(request):
         print("1")
         myFile = request.FILES.get('file-7[]', None)  # 获取上传的文件，没有默认为None
         if not myFile:#判断文件是否为空
-            print("2")
             return JsonResponse({"result": "No File For Upload"})
         if re.match("^.+\\.(?i)(pdf)$", myFile.name):  # 判断一下文件是否为PDF文件，如果是PDF通过，不是拒绝上传
-            print("3")
             print(os.path.exists('statics/filepath/'+myFile.name))
             if not os.path.exists('statics/filepath/' + myFile.name):#判断文件是否已经存在
-                print("4")
                 classify = request.POST.getlist('classify')  # checkbox类型的取值
                 classifyStr = "".join(classify)  # 将list转成字符串输出
-                print('myValues', classify)
                 loadtime = time.strftime("%Y-%m-%d", time.localtime())  # 获取时间年-月-日
-                print("时间", loadtime)
                 # if not myFile:
                 #     return HttpResponse("no file for upload")
                 destination = open(os.path.join("statics/filepath", myFile.name), "wb+")  # 创建了一个file对象
                 try:
                     file = models.Filename.objects.create(name=myFile.name, classify=classifyStr, time=loadtime)  # 将文件名存进数据库表中
-                except:
-                    print("上传失败")
-                    return JsonResponse({'result':'File Upload Fail'})
-                print(file.name)
+                except:                  
+                    return JsonResponse({'result':'File Upload Fail'})           
                 for chunk in myFile.chunks():  # 分块写入文件
                     destination.write(chunk)
                 destination.close()  # 关闭
@@ -53,7 +45,6 @@ def fileload(request):
                 return JsonResponse({'result':'Upload Success'})
             else:
                 return JsonResponse({'result':'File Already Exists'})
-
         else:
             return JsonResponse({"result": "FileTypeError"})
 
@@ -91,9 +82,7 @@ def fileview(request):
     fileinfo = models.Filename.objects.all()
     nametime = {}
     for i in fileinfo:
-        print(i.name,i.time)
         nametime[i.name] = i.time #给字典赋值
-    print(nametime)
     Fnametime = list(nametime.items())
     Fnametime.reverse()
     contacts = paging(request,Fnametime)#分页
@@ -105,18 +94,14 @@ def fileview(request):
     #     contacts = paginator.page(1)
     # except EmptyPage:
     #     contacts = paginator.page(paginator.num_pages)
-    print(request.user.is_authenticated)
     if request.user.is_authenticated:#判断用户是否登录
         Islogin = True
     else:
         Islogin = False
-
     return render(request,'file_list.html',{'data':contacts,'user':Islogin})
 #下载
 def filedown(request):
     filename = "".join( request.GET.getlist('name'))
-    print(filename)
-    print("你好我在filedown中")
     file = open('statics/filepath/'+filename,'rb')
     response = FileResponse(file)
     response['Content-Type'] = 'application/octet-stream'
@@ -124,9 +109,7 @@ def filedown(request):
     return response
 #关键字搜索
 def keyQuery(request):
-    print("我在keyQuery中")
     keyname = request.POST.get('keyvalue')
-    print('keyname:',keyname)
     filename = models.Filename.objects.filter(name__contains=keyname).all()
     nametime = {}
     for i in filename:
@@ -134,12 +117,10 @@ def keyQuery(request):
     Fnametime = list(nametime.items())
     Fnametime.reverse()
     contacts = paging(request,Fnametime)
-    print(nametime)
     if request.user.is_authenticated:  # 判断用户是否登录
         Islogin = True
     else:
         Islogin = False
-    print('islogin:%s'%Islogin)
     return render(request,"file_list.html",{'data':contacts,'user':Islogin})
 #按照条件查询
 def queryfile(request):
@@ -164,8 +145,6 @@ def loginManager(request):
 
 #用户登录验证
 def loginVerify(request):
-    print("我在loginVerify中")
-    print(request)
     username = "".join(request.POST.getlist("username"))#取到ajax传过来的username值并转化成字符串
     password = "".join(request.POST.getlist("password"))#取到ajax传过来的password值并转化成字符串
     user = authenticate(username=username,password=password)
@@ -195,7 +174,6 @@ def logoutuser(request):
 #删除数据库的记录，以及filepath文件夹下的文件
 def deletefile(request):
     filename = request.GET.get("filename")#获取要删除文件的名称
-    print(filename)
     if os.path.exists("statics/filepath/"+filename):#判断文件是否存在
         deleteobj = models.Filename.objects.filter(name=filename)#去数据库查找文件记录
         if deleteobj:#判断数据库是否存在文件
